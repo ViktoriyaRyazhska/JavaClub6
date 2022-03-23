2ed22153957a5fa2bdd3f5fa5f3477c05bf2fe67
AndriyMakoveichuk <andriy.makoveychuk@gmail.com>
2022-03-23 16:11:32 +0200

    Task 5 and 9

---
+++ main/java/com.ss.ita/basic/Task9.java
@@ -0,0 +1,11 @@
+public class Kata {
+
+    public static int makeNegative(final int x) {
+
+        if (x > 0) {
+            return -x;
+        } else {
+            return x;
+        }
+    }
+}
