9357a18e61568c504906616b4432228b663eb61d
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-03-23 22:16:50 +0200

    first task

---
+++ Menu.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Menu {
+    Scanner scanner = new Scanner(System.in);
+    ;
+
+    public void chooseTask() {
+        System.out.println("Choose the number of tasks:");
+        int chooseNumber = scanner.nextInt();
+        switch (chooseNumber) {
+            case 1:
+                System.out.print("Put the data, i= ");
+                int putData = scanner.nextInt();
+                doubleInteger(putData);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+        }
+    }
+
+    public static int doubleInteger(int i) {
+        System.out.println("Task1:Code as fast as you can! You need to double the integer and return it.");
+        int result=i * i;
+        System.out.println(result);
+        return result;
+
+    }
+}
+
