189359799ffc4ff61963ad23ed35cdccbb4c0f4d
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-03-24 08:53:15 +0200

    hello manu and main class added

--- src/Basic/Task10.java
+++ src/Basic/Task10.java
@@ -2,13 +2,11 @@
 //        https://www.codewars.com/kata/5803956ddb07c5c74200144e/train/java
 //        Author:Maksim Dziurei
 
-package Basic;
-
 import java.util.Scanner;
 
 public class Task10 {
 
-    public static void main(String[] args) {
+    public static void ageCounter (String[] args) {
         int min, max, age;
         Scanner sc = new Scanner(System.in);
         System.out.println("            ---WELCOME---");
