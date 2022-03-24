6c00e09d99e94002cf76eff6cc7677420f28f0a0
kubrub <keshka3455@gmail.com>
2022-03-24 20:39:37 +0200

    added task26 convert boolean values to strings yes or no

---
+++ src/main/java/Conditions/Task26.java
@@ -0,0 +1,13 @@
+package Conditions;
+
+public class Task26 {
+    public static String boolToWord(boolean b)
+    {
+        if(b == true){
+            return "Yes";
+        }
+        else{
+            return "No";
+        }
+    }
+}
