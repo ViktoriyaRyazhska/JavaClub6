553b86449e1e05a1f5fbc1ebff236ddf0e4fee3f
AndriyMakoveichuk <andriy.makoveychuk@gmail.com>
2022-03-24 13:36:33 +0200

    Task 29, 33 and 37

---
+++ main/java/com.ss.ita/basic/Task33.java
@@ -0,0 +1,24 @@
+public class Dinglemouse {
+
+  public static int[] humanYearsCatYearsDogYears(final int humanYears) {
+    // Your code here!
+    int catYears = 0;
+    int dogYears = 0;
+
+    for (int i = 1; i <= humanYears; i++) {
+      if (i == 1) {
+        catYears+=11;
+        dogYears+=10;
+      }
+      if (i == 2) {
+        catYears+=5;
+        dogYears+=4;
+      }
+
+      catYears+=4;
+      dogYears+=5;
+
+    }
+    return new int[]{humanYears,catYears,dogYears};
+  }
+}
---
+++ main/java/com.ss.ita/basic/Task37.java
@@ -0,0 +1,12 @@
+public class Sequence{
+
+  public static int[] reverse(int n){
+
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      array[i] = n - i;
+    }
+    return array;
+  }
+}
