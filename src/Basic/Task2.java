709f03f6dba6b6ce01e02eb39943483315efb5f2
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-03-25 14:06:55 +0200

    Task2 done

---
+++ src/Basic/Task2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void multiply() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("            ---WELCOME---");
+        System.out.println("Implement a function which multiplies two numbers.");
+        System.out.print("Put the data,num1= " );
+        int num1=scanner.nextInt();
+        System.out.print("num2= ");
+        int num2=scanner.nextInt();
+        System.out.println(num1*num2);
+    }
+
+}
