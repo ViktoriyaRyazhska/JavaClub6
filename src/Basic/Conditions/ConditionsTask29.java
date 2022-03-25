5c32a2d185f3f789519e507ae1047b664d446cd1
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-03-25 18:48:07 +0200

    Task29 done

--- src/Basic/Conditions/ConditionsTask29.java
+++ src/Basic/Conditions/ConditionsTask29.java
@@ -3,18 +3,23 @@ package Conditions;
 import java.util.Scanner;
 
 public class ConditionsTask29 {
-    public static int sequence(int n) {
+    public static void sequence() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Consider a sequence where the first two numbers are 0 and 1 and the next number \n" +
                 "of the sequence is the sum of the previous 2 modulo 3.\n" +
                 " Write a function that finds the nth number of the sequence.");
-        System.out.println("N=");
-//         n = scanner.nextInt();
-        if (n<=1){
-            return n;
-        } return sequence(n-1)+sequence(n-2);
-
+        System.out.print("n=");
+        int n = scanner.nextInt();
+        if (n <= 1) {
+            System.out.println(n);
         }
+        int[] f = new int[n];
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+        System.out.println(f[n-1]);
     }
-
+}
 
