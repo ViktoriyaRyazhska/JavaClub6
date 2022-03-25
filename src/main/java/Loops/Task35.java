08b9e1c16e3edaa8d9c3a425e158b365ded4e5a3
Taras Kadushkevych <kadtaras@gmail.com>
2022-03-25 17:50:36 +0200

    task21, task 35

---
+++ src/main/java/Loops/Task35.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class Task35 {
+
+
+    public static int[] map(int[] arr) {
+
+        for (int i=0; i<arr.length; i++){
+            arr[i] = arr[i]*2;
+        }
+        return arr;
+    }
+
+
+}
