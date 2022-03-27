16f844feb16219fc30cf0f96ce25760a1f5b934c
MaxKl <57271864+MusknotMax@users.noreply.github.com>
2022-03-27 20:21:27 +0300

    Added t55

---
+++ src/Basic/StringProgram/StringProgramTask55.java
@@ -0,0 +1,19 @@
+//Opposite number
+//https://www.codewars.com/kata/opposite-number/
+//Author:Maksim Dziurei
+
+
+package StringProgram;
+
+import java.util.Scanner;
+
+public class StringProgramTask55 {
+    public static Integer oppositeNum(){
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Opposite number");
+        System.out.print("Enter your number:");
+        number = sc.nextInt();
+        return -1*number;
+    }
+}
