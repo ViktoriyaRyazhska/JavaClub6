0b7f1a282e109a269e21e38f1d5624311c695f1b
NataliaHlazova <nataskaglazova@gmail.com>
2022-03-29 17:50:14 +0300

    team5

---
+++ src/main/java/Basic/task1.java
@@ -0,0 +1,12 @@
+package Basic;
+
+class task1 {
+    public static int doubleInteger(int i) {
+        // Double the integer and return it!
+        return i * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(2));
+    }
+}
