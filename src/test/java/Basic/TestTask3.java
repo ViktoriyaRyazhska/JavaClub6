1d3080e4e6d21627eeb9b12b88d3f3b7f2342e93
kubrub <keshka3455@gmail.com>
2022-03-29 15:00:41 +0300

    added test for task3 get Volume Of Cuboid

--- src/test/java/Basic/TestTask3.java
+++ src/test/java/Basic/TestTask3.java
@@ -1,2 +1,17 @@
-package Basic;public class TestTask3 {
+package Basic;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+public class TestTask3 {
+    private static final double delta = 0.0001;
+    @Test
+    public void examples() {
+        // assertEquals("expected", "actual");
+
+        assertEquals(4, Task3.getVolumeOfCuboid(1,2,2), delta);
+        assertEquals(63, Task3.getVolumeOfCuboid(6.3, 2, 5), delta);
+    }
 }
+
