8c49ce7cf3ca28b13d193b49d52cfb6e27c3af9b
NataliaHlazova <nataskaglazova@gmail.com>
2022-03-29 18:35:55 +0300

    collections. task1

--- src/main/java/Collections/task1.java
+++ src/main/java/Collections/task1.java
@@ -1,5 +1,18 @@
 package Collections;
 
-public class task1 {
+import java.util.List;
+import java.util.ArrayList;
 
+public class task1 {
+    public static List<String> added() {
+        List<String> items = new ArrayList<>();
+        items.add("obj1");
+        items.add("obj2");
+        items.add("obj3");
+        return items;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(added());
+    }
 }
