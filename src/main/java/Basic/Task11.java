9c759adfc63088b38f9ce86a088d1b6fa0d40095
prystaikoroman <romapryst@yahoo.com>
2022-03-29 19:19:26 +0300

    complete Debug celsius converter

---
+++ src/main/java/Basic/Task11.java
@@ -0,0 +1,22 @@
+package Basic;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+        System.out.println("10.0 is above freezing temperature" + " "+ weatherInfo(50));
+    }
+    public static String weatherInfo(int temp) {
+        double c = convertToCelsius(temp);
+        if (c < 0)
+            return (c + " is freezing temperature");
+        else
+            return (c + " is above freezing temperature");
+    }
+
+    public static double convertToCelsius(int temperature) {
+        double celsius = (temperature - 32) * (5/9.0);
+        return celsius;
+    }
+
+
+}
