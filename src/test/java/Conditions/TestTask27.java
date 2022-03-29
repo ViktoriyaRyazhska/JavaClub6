5b5e3746c792be1275b17ba4f9d69ac6893e2a29
kubrub <keshka3455@gmail.com>
2022-03-29 15:27:01 +0300

    added test for task27 Will there be enough space?

---
+++ src/test/java/Conditions/TestTask27.java
@@ -0,0 +1,13 @@
+package Conditions;
+
+import org.testng.annotations.Test;
+import static org.junit.Assert.*;
+
+public class TestTask27 {
+    @Test
+    public void testBob() {
+        assertEquals("Should return 0.", 0, Task27.enough(10, 5, 5));
+        assertEquals("Should return 10.", 10, Task27.enough(100, 60, 50));
+        assertEquals("Should return 0.", 0, Task27.enough(20, 5, 5));
+    }
+}
