5d82cf913aa54c8e90184328536dde6684361c4c
Taras Kadushkevych <kadtaras@gmail.com>
2022-03-29 12:58:59 +0300

    task 19, 23, 59, 67

---
+++ src/main/java/String/Task59.java
@@ -0,0 +1,92 @@
+package String;
+
+public class Task59 {
+    public static String position(char alphabet)
+    {
+
+        int charPosition=0;
+
+        switch(alphabet) {
+            case 'a':
+                charPosition = 1;
+                break;
+            case 'b':
+                charPosition = 2;
+                break;
+            case 'c':
+                charPosition = 3;
+                break;
+            case 'd':
+                charPosition = 4;
+                break;
+            case 'e':
+                charPosition = 5;
+                break;
+            case 'f':
+                charPosition = 6;
+                break;
+            case 'g':
+                charPosition = 7;
+                break;
+            case 'h':
+                charPosition = 8;
+                break;
+            case 'i':
+                charPosition = 9;
+                break;
+            case 'j':
+                charPosition = 10;
+                break;
+            case 'k':
+                charPosition = 11;
+                break;
+            case 'l':
+                charPosition = 12;
+                break;
+            case 'm':
+                charPosition = 13;
+                break;
+            case 'n':
+                charPosition = 14;
+                break;
+            case 'o':
+                charPosition = 15;
+                break;
+            case 'p':
+                charPosition = 16;
+                break;
+            case 'q':
+                charPosition = 17;
+                break;
+            case 'r':
+                charPosition = 18;
+                break;
+            case 's':
+                charPosition = 19;
+                break;
+            case 't':
+                charPosition = 20;
+                break;
+            case 'u':
+                charPosition = 21;
+                break;
+            case 'v':
+                charPosition = 22;
+                break;
+            case 'w':
+                charPosition = 23;
+                break;
+            case 'x':
+                charPosition = 24;
+                break;
+            case 'y':
+                charPosition = 25;
+                break;
+            case 'z':
+                charPosition = 26;
+                break;
+        }
+        return "Position of alphabet: "+charPosition;
+
+    }
+}
