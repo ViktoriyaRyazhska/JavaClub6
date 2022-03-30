994762538f9816a5f8b95b41781b7c144e7224de
prystaikoroman <romapryst@yahoo.com>
2022-03-30 19:36:44 +0300

    complete Replace all dots

---
+++ src/main/java/String/Task5.java
@@ -0,0 +1,9 @@
+package String;
+
+public class Task5 {
+//    https://www.codewars.com/kata/596c6eb85b0f515834000049/train/java
+
+    public static String replaceDots(final String str) {
+        return str.replaceAll("\\.", "-");
+    }
+}
