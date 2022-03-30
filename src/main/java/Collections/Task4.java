592f6e3f74eda9c050e1a2202dde410b96d9fd71
MaksymKorolkov <max12xit02@gmail.com>
2022-03-30 18:43:51 +0300

    added Collections. Task 4

--- src/main/java/Collections/Task4.java
+++ src/main/java/Collections/Task4.java
@@ -1,4 +1,17 @@
 package Collections;
 
-public class Task4 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task4{
+    public class Dictionary{
+        Map<String, String> words = new HashMap<String, String>();
+        public void newEntry(String word, String entry){
+            words.put(word, entry);
+        }
+        public String look(String word){
+            if(words.get(word) != null) return words.get(word);
+            return "Cant find entry for " + word;
+        }
+    }
 }
