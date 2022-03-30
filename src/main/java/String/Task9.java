2943870967ae0156e3a31433a8792a57d3928b2b
prystaikoroman <romapryst@yahoo.com>
2022-03-30 19:48:32 +0300

    complete Find the position!

---
+++ src/main/java/String/Task9.java
@@ -0,0 +1,9 @@
+package String;
+
+public class Task9 {
+    //    https://www.codewars.com/kata/find-the-position/
+
+    public static String position(char alphabet) {
+        return "Position of alphabet: "+ (((int)alphabet)-96);
+    }
+}
