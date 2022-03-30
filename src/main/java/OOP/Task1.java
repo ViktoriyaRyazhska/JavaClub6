5aac1acc9cb9b0c8393d3760655cf3824079de71
prystaikoroman <romapryst@yahoo.com>
2022-03-30 19:11:21 +0300

    complete subclasses - Adam and Eve

---
+++ src/main/java/OOP/Task1.java
@@ -0,0 +1,24 @@
+package OOP;
+
+public class Task1 {
+//    https://www.codewars.com/kata/547274e24481cfc469000416/train/java
+}
+ class God {
+    public static Human[] create(){
+        Human[] par = new Human[2];
+        par[0] = new Man();
+        par[1] = new Woman();
+        return par;
+    }
+}
+class Human{
+
+}
+class Man extends Human{
+
+}
+class Woman extends Human{
+
+}
+//code
+
