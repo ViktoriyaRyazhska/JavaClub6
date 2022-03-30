6bced69d1b4e6e901f54c4d538cd5491531d4f23
MaksymKorolkov <max12xit02@gmail.com>
2022-03-30 17:39:38 +0300

    added Loops. Task 11

--- src/main/java/Loops/Task11.java
+++ src/main/java/Loops/Task11.java
@@ -1,4 +1,20 @@
 package Loops;
 
 public class Task11 {
+    public static int twiceAsOld(int fatherAge, int sonAge){
+        int age = fatherAge;
+        if (fatherAge * 1.0 / 2 > sonAge){
+            while (fatherAge * 1.0 / 2 != sonAge){
+                fatherAge++;
+                sonAge++;
+            }
+            return fatherAge - age;
+        } else {
+            while (fatherAge * 1.0 / 2 != sonAge){
+                fatherAge--;
+                sonAge--;
+            }
+            return age - fatherAge;
+        }
+    }
 }
