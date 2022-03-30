1f091e7122237c9db498a2bc393a7abb08f833f2
MaksymKorolkov <max12xit02@gmail.com>
2022-03-30 17:12:01 +0300

    added Classes. Task 6

--- src/main/java/Classes/Task6.java
+++ src/main/java/Classes/Task6.java
@@ -1,4 +1,38 @@
 package Classes;
 
 public class Task6 {
+    public static class Block{
+        private int width;
+        private int length;
+        private int height;
+        Block(int[] proportions){
+            setWidth(proportions[0]);
+            setLength(proportions[1]);
+            setHeight(proportions[2]);
+        }
+        void setWidth(int Width){
+            width = Width;
+        }
+        void setLength(int Length){
+            length = Length;
+        }
+        void setHeight(int Height){
+            height = Height;
+        }
+        int getWidth(){
+            return width;
+        }
+        int getLength(){
+            return length;
+        }
+        int getHeight(){
+            return height;
+        }
+        int getVolume(){
+            return width * height * length;
+        }
+        int getSurfaceArea(){
+            return 2 * (width * length + width * height + length * height);
+        }
+    }
 }
