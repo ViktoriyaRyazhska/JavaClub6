927bce4704a836bae0ace0ca8c01cb2c6f044c21
MaksymKorolkov <max12xit02@gmail.com>
2022-03-30 17:15:32 +0300

    added Conditions. Task 1

--- src/main/java/Conditions/Task1.java
+++ src/main/java/Conditions/Task1.java
@@ -1,4 +1,10 @@
 package Conditions;
 
 public class Task1 {
+    public int getOpposite(int number){
+        return number * (-1);
+    }
+    public float getOpposite(float number){
+        return number * (-1);
+    }
 }
