63cd677d1765bfb232612db111206398933c1812
prystaikoroman <romapryst@yahoo.com>
2022-03-30 20:00:42 +0300

    complete Abbreviate a Two Word Name

---
+++ src/main/java/String/Task13.java
@@ -0,0 +1,14 @@
+package String;
+
+import java.util.Locale;
+
+public class Task13 {
+//    https://www.codewars.com/kata/abbreviate-a-two-word-name/
+
+    public static String abbrevName(String name) {
+        String result;
+        final String[] split = name.split(" ");
+        result = (split[0].charAt(0) + "." + split[1].charAt(0)).toUpperCase();
+        return result;
+    }
+}
