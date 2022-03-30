117e4182e238814f2627a706b74e53a4f45a1e98
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-03-30 21:51:04 +0300

    Menu was refactored, added exit

--- src/Basic/Main/FirstHelloMenu.java
+++ src/Basic/Main/FirstHelloMenu.java
@@ -12,9 +12,10 @@ import java.util.Scanner;
 
 public class FirstHelloMenu {
     Scanner scanner = new Scanner(System.in);
-
-    public void chooseTopicOfTask() {
-        System.out.println("WELCOME");
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public void helloMenu() {
+        System.out.println(ANSI_PURPLE+"WELCOME TO MAIN MENU");
         System.out.println("CHOOSE THE NUMBER OF TOPIC:");
         System.out.println("BASIC PROGRAM -1");
         System.out.println("CLASSES-2");
@@ -23,29 +24,42 @@ public class FirstHelloMenu {
         System.out.println("STRING-5");
         System.out.println("OOP-6");
         System.out.println("COLLECTIONS-7");
-        int chooseNumber = scanner.nextInt();
-        switch (chooseNumber) {
-            case 1:
-                BasicProgramMenu.chooseTask();
-                break;
-            case 2:
-                ClassesMenu.chooseTask();
-                break;
-            case 3:
-                ConditionsMenu.chooseTask();
-                break;
-            case 4:
-                LoopsMenu.chooseTask();
-                break;
-            case 5:
-                StringProgramMenu.chooseTask();
-                break;
-            case 6:
-                OOPMenu.chooseTask();
-                break;
-            case 7:
-                CollectionsMenu.chooseTask();
-                break;
+        System.out.println("EXIT-0"+ANSI_RESET);
+    }
+
+    public void chooseTopicOfTask() {
+        while (true) {
+            helloMenu();
+            int chooseNumber = scanner.nextInt();
+            switch (chooseNumber) {
+                case 1:
+                    BasicProgramMenu.chooseTask();
+                    break;
+                case 2:
+                    ClassesMenu.chooseTask();
+                    break;
+                case 3:
+                    ConditionsMenu.chooseTask();
+                    break;
+                case 4:
+                    LoopsMenu.chooseTask();
+                    break;
+                case 5:
+                    StringProgramMenu.chooseTask();
+                    break;
+                case 6:
+                    OOPMenu.chooseTask();
+                    break;
+                case 7:
+                    CollectionsMenu.chooseTask();
+                    break;
+                case 8:
+                    System.exit(0);
+                default:
+                    System.out.println("Error, wrong answer");
+                    helloMenu();
+
+            }
         }
     }
 }
