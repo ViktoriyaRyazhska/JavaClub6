48cbfa66d90f66dba7e259adf8b9cf489924021c
MaksymKorolkov <max12xit02@gmail.com>
2022-03-30 18:04:57 +0300

    added String. Task 15

--- src/main/java/String/Task15.java
+++ src/main/java/String/Task15.java
@@ -1,4 +1,18 @@
 package String;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Task15 {
+    public static String SortAndStar(String[] vector){
+        List<String> list = Arrays.asList(vector);
+        Collections.sort(list);
+        String rawResult = list.get(0);
+        String result = "";
+        for (int i = 0; i < rawResult.length() - 1; i++){
+            result += rawResult.charAt(i) + "***";
+        }
+        return result + rawResult.charAt(rawResult.length() - 1);
+    }
 }
