61cbec70c72f069d29fd05cd87d65373abe555ec
NataliaHlazova <87066582+NataliaHlazova@users.noreply.github.com>
2022-03-30 19:06:38 +0300

    Conditions. Task8

---
+++ src/main/java/Conditions/Task8.java
@@ -0,0 +1,15 @@
+package Conditions;
+
+public class Task8 {
+    public static String bonusTime(int salary, boolean bonus) {
+        // if bonus== true => salary * 10 "£" = "\u00A3"
+        // if bonus== false => salary "£" = "\u00A3"
+
+        return bonus == false ? "\u00A3" + salary : "\u00A3" + salary * 10;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bonusTime(4, false));
+    }
+}
