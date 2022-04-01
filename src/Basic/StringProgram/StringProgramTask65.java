2e08de26c3bee2f11660c62d5d9d7ef29ffe7211
MaxKl <57271864+MusknotMax@users.noreply.github.com>
2022-04-01 18:16:15 +0300

    Added task65 and fixed bugs

---
+++ src/Basic/StringProgram/StringProgramTask65.java
@@ -0,0 +1,22 @@
+//Push a hash/an object into array
+//http://www.codewars.com/kata/all-star-code-challenge-number-18/
+//Author:Maksim Dziurei
+
+
+
+package StringProgram;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class StringProgramTask65 {
+
+    public static List<String> push(){
+        System.out.println("Push a hash/an object into array");
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        System.out.println(items);
+        return items;
+    }
+
+}
