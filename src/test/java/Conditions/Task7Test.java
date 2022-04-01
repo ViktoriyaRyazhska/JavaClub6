c0f53bd3f0c36fc560784ddd334e24440ecfe587
prystaikoroman <romapryst@yahoo.com>
2022-04-01 13:41:56 +0300

    complete Conditions\Task7Test

---
+++ src/test/java/Conditions/Task7Test.java
@@ -0,0 +1,21 @@
+package Conditions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
+
+class Task7Test {
+    @Test
+    void testBasicMath() {
+
+        assertEquals(0, Task7.basicMath("qwe", 1, 1).intValue());
+        assertEquals(20, Task7.basicMath((String) "/", 60, 3).intValue());
+        assertEquals(4, Task7.basicMath((String) "-", 5, 1).intValue());
+        assertThat(Task7.basicMath((String) "+", 5, 5), is(10));
+        assertThat( Task7.basicMath((String) "*", 5, 5), is(25));
+        assertThrows(ArithmeticException.class, ()->Task7.basicMath( "/", 1, 0)) ;
+    }
+}
+
