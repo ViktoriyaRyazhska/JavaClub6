ee44fd5c95709a3d7ece799a12ecff3af2010f2f
prystaikoroman <romapryst@yahoo.com>
2022-04-01 12:45:33 +0300

    complete Classes\Task5Test

---
+++ src/test/java/Classes/Task5Test.java
@@ -0,0 +1,26 @@
+package Classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task5Test {
+    @Test
+    void testDeclareWinner() {
+        assertEquals("Lew", Task5.declareWinner(new Fighter("Lew", 10, 2),new Fighter("Harry", 5, 4), "Lew"));
+        assertEquals("Harry", Task5.declareWinner(new Fighter("Lew", 10, 2),new Fighter("Harry", 5, 4), "Harry"));
+        assertEquals("Harald", Task5.declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
+    }
+
+
+    @Test
+    void testDeclareWinner1() {
+        Fighter fighter = new Fighter("Name1", 1, 1);
+        Fighter fighter1 = new Fighter("Name2", 1, 0);
+
+        assertEquals("Name1", Task5.declareWinner(fighter, fighter1, "Name2"));
+        assertEquals(1, fighter.health);
+        assertEquals(0, fighter1.health);
+    }
+
+}
