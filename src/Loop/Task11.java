611ec0e70bc2330711ca26ef81a290c2c619cb0e
julia <jkhomyn1381@gmail.com>
2022-04-01 16:53:58 +0300

    Completed Loops Task 11

---
+++ src/Loop/Task11.java
@@ -0,0 +1,13 @@
+public class Task11 {
+
+    public static void main(String[] args) {
+        System.out.println(TwiceAsOld(30, 0));
+        System.out.println(TwiceAsOld(30, 7));
+        System.out.println(TwiceAsOld(45, 30));
+    }
+
+    public static int TwiceAsOld(int dadYears, int sonYears){
+        int twiceAsOld = dadYears - sonYears * 2;
+        return twiceAsOld > 0 ? twiceAsOld : (-1) * twiceAsOld;
+    }
+}
