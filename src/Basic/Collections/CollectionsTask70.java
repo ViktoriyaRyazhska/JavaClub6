266dc68be22b814efc569c438f5f1eb17194570c
MaxKl <57271864+MusknotMax@users.noreply.github.com>
2022-04-01 18:35:48 +0300

    Aded t70

---
+++ src/Basic/Collections/CollectionsTask70.java
@@ -0,0 +1,20 @@
+//Push a hash/an object into array
+//http://www.codewars.com/kata/all-star-code-challenge-number-18/
+//Author:Maksim Dziurei
+
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionsTask70 {
+    public static List<String> push(){
+        System.out.println("Push a hash/an object into array");
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        System.out.println(items);
+        return items;
+    }
+
+
+}
