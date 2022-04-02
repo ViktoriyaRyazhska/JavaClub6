6498eb1effdbfa6aa1e7e33b8bc864fabee26f7f
prystaikoroman <romapryst@yahoo.com>
2022-04-02 14:06:05 +0300

    completed menuLoops

--- src/main/java/AllMenu.java
+++ src/main/java/AllMenu.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 import Basic.*;
 import Classes.*;
 import Conditions.*;
+import Loops.Task5;
 import com.sun.xml.internal.bind.v2.TODO;
+
+import static Loops.Task2.humanYearsCatYearsDogYears;
 
 public class AllMenu {
     static void menuBasic(Scanner sc) {
@@ -314,27 +317,136 @@ public class AllMenu {
         System.out.println();
         System.out.println("=========================Menu=============================");
         System.out.println("Loops:");
-        System.out.println("1 - You need to double the integer and return it.");
-        System.out.println("2 - Implement a function which multiplies two numbers.");
         System.out.println(
-                "3 - Calculate the volume of a cuboid with three values: length, width and the height of the cuboid.");
+                "1 - Write a function called repeatStr which repeats the given string string exactly n times.");
         System.out.println(
-                "4 -  a function is_divisible() that will tell you whether a wall of a certain length can exactly fit an integer number of pixels of a certain length.");
+                "2 - I have a cat and a dog.\n" +
+                "I got them at the same time as kitten/puppy. That was humanYears years ago.\n" +
+                "Return their respective ages now as [humanYears,catYears,dogYears]");
         System.out.println(
-                "5 -  a function that returns a greeting for a user, but greet for Johnny slightly different.");
-        System.out.println("6 - a function to_binary/ToBinary which returns that number in a binary format.");
+                "3 - Return the average of the given array rounded down to its nearest integer.");
         System.out.println(
-                "7 - Clock shows h hours, m minutes and s seconds after midnight. A function which returns the time since midnight in milliseconds.");
-        System.out.println("8 - Given a year, return the century it is in.");
-        System.out.println("9 - Given a number and have to make it negative");
+                "4 - Given an array of integers, return a new array with each value doubled.");
         System.out.println(
-                "10 - Given an integer (1 <= n <= 100) representing a person's age, return their minimum and maximum age range.");
-        System.out.println("11 - Get the celsius converter working properly.");
-        System.out.println("12 - Get the celsius converter working properly.");
-        System.out.println("13 - Get the celsius converter working properly.");
-        System.out.println("14 - Get the celsius converter working properly.");
-        System.out.println("15 - Get the celsius converter working properly.");
+                "5 - Consider an array/list of sheep where some sheep may be missing from their place. \n" +
+                "We need a function that counts the number of sheep present in the array (true means present).");
+        System.out.println(
+                "6 - Build a function that returns an array of integers from n to 1 where n>0.");
+        System.out.println(
+                "7 - You receive an array with your peers' test scores. Now calculate the average and compare your score!");
+        System.out.println(
+                "8 - Given a non-empty array of integers, return the result of multiplying the values together in order.");
+        System.out.println(
+                "9 - You get an array of numbers, return the sum of all of the positives ones.");
+        System.out.println(
+                "10 - Save the animals by switching them back.\n" +
+                        "You will be given an array which will have three values (tail, body, head).\n" +
+                        "It is your job to re-arrange the array so that the animal is the right way round (head, body, tail).");
+        System.out.println("" +
+                "11 - Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).");
+        System.out.println(
+                "12 - Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.");
+        System.out.println(
+                "13 - Write a program that finds the summation of every number from 1 to num. \n" +
+                "The number will always be a positive integer greater than 0.");
+        System.out.println(
+                "14 - The prime numbers are not regularly spaced. For example from 2 to 3 the gap is 1. \n" +
+                "From 3 to 5 the gap is 2. From 7 to 11 it is 4. \n" +
+                "Between 2 and 50 we have the following pairs of 2-gaps primes: 3-5, 5-7, 11-13, 17-19, 29-31, 41-43");
+        System.out.println(
+                "15 - In this game, there are 21 sticks lying in a pile. \n" +
+                "Players take turns taking 1, 2, or 3 sticks. \n" +
+                "The last person to take a stick wins. ");
         System.out.println("16 - Back to Menu");
+
+        while (true) {
+            switch (sc.next()) {
+
+                case "1": {
+                    System.out.println(Loops.Task1.repeatStr(4, "string"));
+                    menuBasic(sc);
+                    break;
+                }
+
+                case "2": {
+                    System.out.println(Arrays.toString(Loops.Task2.humanYearsCatYearsDogYears(10)));
+                    menuBasic(sc);
+                    break;
+                }
+
+                case "3": {
+                    System.out.println(Loops.Task3.getAverage(new int[]{167, -2, 16, 99, 26, 92, 43, -234, 35, 80}));
+                    menuBasic(sc);
+                    break;
+                }
+                case "4": {
+                    System.out.println(Arrays.toString(Loops.Task4.map(new int[]{4, 1, 1, 1, 4})));
+                    menuBasic(sc);
+                    break;
+                }
+                case "5": {
+                    Loops.Task5 task5 = new Task5();
+                    System.out.println(task5.countSheeps(new Boolean[]{false, true, false, true}));
+                    menuBasic(sc);
+                    break;
+                }
+                case "6": {
+                    System.out.println(Arrays.toString(Loops.Task6.reverse(5)));
+                    menuBasic(sc);
+                    break;
+                }
+                case "7": {
+                    System.out.println(Loops.Task7.betterThanAverage(new int[]{3,1,4 ,5,2}, 2) );
+                    menuBasic(sc);
+                    break;
+                }
+                case "8": {
+                    System.out.println(Loops.Task8.grow(new int[]{2, 2, 2, 2, 2, 2}));
+                    menuBasic(sc);
+                    break;
+                }
+                case "9": {
+                    System.out.println(Loops.Task9.sum(new int[]{2, -5, 8,0,-24,12,5,4}));
+                    menuBasic(sc);
+                    break;
+                }
+                case "10": {
+                    System.out.println(Arrays.toString(Loops.Task10.sortEl(new String[]{ "1", "2", "3", "4" })));
+                    menuBasic(sc);
+                    break;
+                }
+                case "11": {
+                    System.out.println(Loops.Task11.TwiceAsOld(39, 5));
+                    menuBasic(sc);
+                    break;
+                }
+                case "12": {
+                    System.out.println(Loops.Task12.nearestSq(111));
+                    menuBasic(sc);
+                    break;
+                }
+                case "13": {
+                    System.out.println(Loops.Task13.summation(5));
+                    menuBasic(sc);
+                    break;
+                }
+                case "14": {
+                    System.out.println(Arrays.toString(Loops.Task14.gap(5, 1245, 65456)));
+                    menuBasic(sc);
+                    break;
+                }
+                case "15": {
+                    System.out.println(Loops.Task15.makeMove(6));
+                    menuBasic(sc);
+                    break;
+                }
+                case "16": {
+                    MainApp.menu();
+                    break;
+                }
+            }
+        }
+
     }
 
 }
