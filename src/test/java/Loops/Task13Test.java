b2ddb3f3a15f6b37c5bbeddff5c48e9ea42b7ed4
prystaikoroman <romapryst@yahoo.com>
2022-04-02 17:49:38 +0300

    completed Loops\Task13Test

---
+++ src/test/java/Loops/Task13Test.java
@@ -0,0 +1,14 @@
+package Loops;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class Task13Test {
+    @Test
+    void testSummation() {
+        assertEquals(1, Task13.summation(1));
+        assertEquals(36, Task13.summation(8));
+    }
+}
+
--- src/test/java/Loops/Task9Test.java
+++ src/test/java/Loops/Task9Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 class Task9Test {
     @Test
     void testSum() {
+
         assertEquals(4, Task9.sum(new int[]{1, 1, 1, 1}));
         assertEquals(3, Task9.sum(new int[]{0, 1, 1, 1}));
         assertEquals(4, Task9.sum(new int[]{1, 1, 1, 1}));
