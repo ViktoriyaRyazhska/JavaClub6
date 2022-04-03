5a43bd99b82ffa1702724f51d566057d8c3ae45c
RoSteik <rostyslav.postolaki@gmail.com>
2022-04-03 12:11:07 +0300

    add changes in Task18 in String

--- src/main/java/String/Task18.java
+++ src/main/java/String/Task18.java
@@ -4,7 +4,7 @@ public class Task18 {
     public static String removeExclamationMarks(String string, int number){
         int i = 0;
         String result = "";
-        while (i != string.length() - 1 || number != 0){
+        while (i != string.length() - 1 && number != 0){
             if (string.charAt(i) == '!') {
                 i++;
                 number--;
