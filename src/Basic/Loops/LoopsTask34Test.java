d336baef2c96588325b0f66e1c017d1a80d23c17
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-03 13:31:48 +0300

    test for task34

---
+++ src/Basic/Loops/LoopsTask34Test.java
@@ -0,0 +1,23 @@
+package Loops;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoopsTask34Test {
+
+        @Test
+        public void one() {
+            assertArrayEquals(new int[]{1,15,15}, LoopsTask34.humanYearsCatYearsDogYears(1));
+        }
+
+        @Test
+        public void two() {
+            assertArrayEquals(new int[]{2,24,24}, LoopsTask34.humanYearsCatYearsDogYears(2));
+        }
+
+        @Test
+        public void ten() {
+            assertArrayEquals(new int[]{10,56,64}, LoopsTask34.humanYearsCatYearsDogYears(10));
+        }
+    }
