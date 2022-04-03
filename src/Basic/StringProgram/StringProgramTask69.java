47a6f7696d7b36c3b584fa6fa6c988213216a8be
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-03 20:30:45 +0300

    test for task 64 and 69

--- src/Basic/StringProgram/StringProgramTask69.java
+++ src/Basic/StringProgram/StringProgramTask69.java
@@ -6,9 +6,9 @@ public class StringProgramTask69 {
     public static String remove() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Remove n exclamation marks in the sentence from left to right. n is positive integer.");
-        System.out.print("Write a sentences witch include exclamation ");
+        System.out.println("Write a sentences witch include exclamation: ");
         String s = scanner.nextLine();
-        System.out.print("How many exclamations should be remove? - ");
+        System.out.println("How many exclamations should be remove? - ");
         int n = scanner.nextInt();
         for (int j = 0; j < n; j++)
             s = s.replaceFirst("!", "");
