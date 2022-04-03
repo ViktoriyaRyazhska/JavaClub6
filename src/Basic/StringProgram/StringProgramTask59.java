28b5235bab548940c212648115286fb38008fce7
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-03 20:21:55 +0300

    test for task 59

--- src/Basic/StringProgram/StringProgramTask59.java
+++ src/Basic/StringProgram/StringProgramTask59.java
@@ -8,11 +8,11 @@ public class StringProgramTask59 {
         System.out.println("Create a function that will return a string that combines all of the letters of the three \n"+
                 "inputed strings in groups. Taking the first letter of all of the inputs and grouping them \n"+
                 "next to each other. Do this for every letter, see example below!\n");
-        System.out.print("First word");
+        System.out.println("First word");
         String one=scanner.next();
-        System.out.print("Second word");
+        System.out.println("Second word");
         String two=scanner.next();
-        System.out.print("Third word");
+        System.out.println("Third word");
         String three=scanner.next();
         String[] part1 = one.split("");
         String[] part2 = two.split("");
@@ -22,7 +22,7 @@ public class StringProgramTask59 {
         for(int i = 0; i < part1.length; i++){
             res+=part1[i]+part2[i]+part3[i];
         }
-        System.out.println(res);
+        System.out.println("result: "+res);
         return res;
     }
 
