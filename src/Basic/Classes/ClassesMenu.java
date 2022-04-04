3c96325e013cf645b1211f4f203bd09f5b8ab56b
MaxKl <57271864+MusknotMax@users.noreply.github.com>
2022-04-04 12:49:26 +0300

    added t15

--- src/Basic/Classes/ClassesMenu.java
+++ src/Basic/Classes/ClassesMenu.java
@@ -16,6 +16,7 @@ public class ClassesMenu {
                 ClassMenuTask14main.resTask14(scanner);
                 break;
             case 15:
+                new ClassTask15.Canvas(7,7).draw(1,1,5,4).drawCanvas();
                 break;
             case 16:
                 ClassTask16.declareWinner(scanner);
