ff92dcb12d52e10e5af7afd848adcdf2b116492a
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-05 12:44:15 +0300

    modified menus

--- src/Basic/StringProgram/StringProgramMenu.java
+++ src/Basic/StringProgram/StringProgramMenu.java
@@ -1,7 +1,7 @@
 package StringProgram;
 
 import Main.Main;
-
+import static Main.FirstHelloMenu.sorryMessage;
 import java.util.Scanner;
 
 public class StringProgramMenu {
@@ -9,54 +9,28 @@ public class StringProgramMenu {
         System.out.println("Choose the number of tasks from 52 to 69:");
         int chooseNumber = scanner.nextInt();
         switch (chooseNumber) {
-            case 52:
-                break;
-            case 53:
-                break;
-            case 54:
-                StringProgramTask54.sayHello();
-                break;
-            case 55:
-                StringProgramTask55.oppositeNum();
-                break;
-            case 56:
-                StringProgramTask56.replaceDots();
-                break;
-            case 57:
-                break;
-            case 58:
-                break;
-            case 59:
-                StringProgramTask59.tripleTrouble();
-                break;
-            case 60:
-                StringProgramTask60.findThePosition();
-                break;
-            case 61:
-                StringProgramTask61.fakeBin();
-                break;
-            case 62:
-                break;
-            case 63:
-                break;
-            case 64:
-                StringProgramTask64.abbrevName();
-                break;
-            case 65:
-                break;
-            case 66:
-                StringProgramTask66.twoSort();
-                break;
-            case 67:
-                break;
-            case 68:
-                break;
-            case 69:
-                StringProgramTask69.remove();
-                break;
-            default:
-                System.out.println(Main.RED_UNDERLINED +"Error, wrong answer"+Main.ANSI_RESET);
+            case 52 -> sorryMessage(52);
+            case 53 -> sorryMessage(53);
+            case 54 -> StringProgramTask54.sayHello();
+            case 55 -> StringProgramTask55.oppositeNum();
+            case 56 -> StringProgramTask56.replaceDots();
+            case 57 -> sorryMessage(57);
+            case 58 -> sorryMessage(58);
+            case 59 -> StringProgramTask59.tripleTrouble();
+            case 60 -> StringProgramTask60.findThePosition();
+            case 61 -> StringProgramTask61.fakeBin();
+            case 62 -> sorryMessage(62);
+            case 63 -> sorryMessage(63);
+            case 64 -> StringProgramTask64.abbrevName();
+            case 65 -> sorryMessage(65);
+            case 66 -> StringProgramTask66.twoSort();
+            case 67 -> sorryMessage(67);
+            case 68 -> sorryMessage(68);
+            case 69 -> StringProgramTask69.remove();
+            default -> {
+                System.out.println(Main.RED_UNDERLINED + "Error, wrong answer" + Main.ANSI_RESET);
                 chooseTask(scanner);
+            }
         }
     }
 }
