583e99cbbf82177e87f200df7d6ea5c0ef2ac38f
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-05 10:28:43 +0300

    task 22 ant test for it

--- src/Basic/Conditions/ConditionsTask22.java
+++ src/Basic/Conditions/ConditionsTask22.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class ConditionsTask22 {
     public static String bonusTime() {
-        System.out.println("It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?\n" +
-                "\n" +
-                "Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.\n" +
-                "\n" +
-                "If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.\n" +
-                "\n" +
-                "Return the total figure the individual will receive as a string prefixed with \"£\"\n");
+        System.out.println("""
+                It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+
+                Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+
+                If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+
+                Return the total figure the individual will receive as a string prefixed with "£"
+                """);
         System.out.print("Enter your salary: ");
         Scanner sc = new Scanner(System.in);
         int salary = sc.nextInt();
