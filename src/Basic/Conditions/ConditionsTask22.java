516413ff05a51a2652536cf8eedff4962ba55c0c
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-05 10:30:42 +0300

    task 22 ant test for it

--- src/Basic/Conditions/ConditionsTask22.java
+++ src/Basic/Conditions/ConditionsTask22.java
@@ -11,8 +11,7 @@ public class ConditionsTask22 {
 
                 If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
 
-                Return the total figure the individual will receive as a string prefixed with "£"
-                """);
+                Return the total figure the individual will receive as a string prefixed with "£"""+"\n");
         System.out.print("Enter your salary: ");
         Scanner sc = new Scanner(System.in);
         int salary = sc.nextInt();
