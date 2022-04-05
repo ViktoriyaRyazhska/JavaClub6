1e602dd6e111db1b25f838a6a65f3470a25804ba
Ivanna Fryshchyn <ivanna.fryshchyn@gmail.com>
2022-04-05 12:41:09 +0300

    modified menus

--- src/Basic/OOP/OOPMenu.java
+++ src/Basic/OOP/OOPMenu.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import static Main.FirstHelloMenu.sorryMessage;
+
 import java.util.Scanner;
 
 public class OOPMenu {
@@ -7,21 +9,14 @@ public class OOPMenu {
         System.out.println("Choose the number of tasks from 48 to 51:");
         int chooseNumber = scanner.nextInt();
         switch (chooseNumber) {
-            case 48:
-                break;
-            case 49:
-                System.out.println("We are so sorry, but this task currently doesn't work\n"+
-        "Please choose another one");
-                break;
-            case 50:
-                OOPTask50.Leetspeak.encode(scanner);
-                break;
-            case 51:
-                 new OOPTask51.Calculator().calc(scanner);
-                break;
-            default:
+            case 48 -> sorryMessage(48);
+            case 49 -> sorryMessage(49);
+            case 50 -> OOPTask50.Leetspeak.encode(scanner);
+            case 51 -> new OOPTask51.Calculator().calc(scanner);
+            default -> {
                 System.out.println("Error, wrong answer, put number from 48 to 51");
                 chooseTask(scanner);
+            }
         }
     }
 }
