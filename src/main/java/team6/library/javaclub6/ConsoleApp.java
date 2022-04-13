a3dfd9cfd67eb967faab50409dc9a277d5d1e471
MaksymKorolkov <max12xit02@gmail.com>
2022-04-13 21:38:05 +0300

    commit

--- src/main/java/team6/library/javaclub6/ConsoleApp.java
+++ src/main/java/team6/library/javaclub6/ConsoleApp.java
@@ -12,13 +12,7 @@ import java.util.List;
 public class ConsoleApp {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().configure();
-        configuration.addAnnotatedClass(Author.class);
-        configuration.addAnnotatedClass(AuthorBook.class);
         configuration.addAnnotatedClass(Book.class);
-        configuration.addAnnotatedClass(RequestStatus.class);
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(UserBook.class);
-        configuration.addAnnotatedClass(UserRole.class);
         StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
         SessionFactory sf = configuration.buildSessionFactory(ssrb.build());
         //Create user
--- src/main/java/team6/library/javaclub6/config/AppInitializer.java
+++ src/main/java/team6/library/javaclub6/config/AppInitializer.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[]{HibernateConfig.class};
     }
 
     @Override
