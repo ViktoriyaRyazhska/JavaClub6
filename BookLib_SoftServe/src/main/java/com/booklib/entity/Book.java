5f23d995f11e84db0f85170a2ef2a96d4f33a3f6
NataliaHlazova <87066582+NataliaHlazova@users.noreply.github.com>
2022-04-14 23:22:50 +0300

    Update Book.java

--- BookLib_SoftServe/src/main/java/com/booklib/entity/Book.java
+++ BookLib_SoftServe/src/main/java/com/booklib/entity/Book.java
@@ -1,27 +1,25 @@
 package com.booklib.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "book")
 public class Book {
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    // @Column(nullable = false, length = 250)
-    // private String title;
+    @Column(nullable = false, length = 250, name = "title")
+    private String title;
 
-    // @OneToMany()
-    // private Set<Genre> genres;
+    @Column(name = "genre")
+    private String genre;
 
+    @Column(name = "copies")
+    private int copies;
 }
