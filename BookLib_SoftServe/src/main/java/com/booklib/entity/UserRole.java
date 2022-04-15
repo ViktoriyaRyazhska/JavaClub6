3e9cfd01c0fca4fe647317d2f1cc1dac95d69f48
p1ktur <91064955+p1ktur@users.noreply.github.com>
2022-04-15 13:49:58 +0300

    finished notification and userrole tables

--- BookLib_SoftServe/src/main/java/com/booklib/entity/UserRole.java
+++ BookLib_SoftServe/src/main/java/com/booklib/entity/UserRole.java
@@ -2,6 +2,7 @@ package com.booklib.entity;
 
 import javax.persistence.*;
 import lombok.*;
+import org.springframework.context.annotation.Primary;
 
 @Entity
 @Setter
@@ -14,5 +15,38 @@ public class UserRole {
 
     @Column(name = "role")
     private String role;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+
+        UserRole other = (UserRole) obj;
+
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        return true;
+    }
 
 }
--- BookLib_SoftServe/src/main/webApp/WEB-INF/views/editUsers.jsp
+++ BookLib_SoftServe/src/main/webApp/WEB-INF/views/editUsers.jsp
@@ -2,7 +2,7 @@
   pageEncoding="ISO-8859-1"%>
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<!DOCTYPE html>
+<!DOC TYPE html>
 <html>
     <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
