c8402ab7e9a5c164467f77f2173094de401dd510
Max <maksym.dovhopolyi.knm.2019@lpnu.ua>
2022-04-18 21:52:53 +0300

    models Author, BookAuthor

--- src/main/java/model/Author.java
+++ src/main/java/model/Author.java
@@ -1,4 +1,48 @@
 package model;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+@Entity
+@Table(name = "author")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "author_id")
+    private Long authorId;
+
+    @Column(name = "author_first_name", length = 50)
+    @NotEmpty()
+    private String authorFirstName;
+
+    @Column(name = "author_last_name", length = 50)
+    @NotEmpty()
+    private String authorLastName;
+
+    public Long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthorFirstName() {
+        return authorFirstName;
+    }
+
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
+    }
+
+    public String getAuthorLastName() {
+        return authorLastName;
+    }
+
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
+    }
 }
--- src/main/java/model/BookAuthor.java
+++ src/main/java/model/BookAuthor.java
@@ -1,4 +1,57 @@
 package model;
-
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+@Entity
+@Table(name = "book_author")
 public class BookAuthor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_author_id")
+    private Long bookAuthorId;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author authorId;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "id")
+    private Book bookId;
+
+    @Column(name = "main_author")
+    @NotEmpty()
+    private boolean mainAuthor;
+
+    public Long getBookAuthorId() {
+        return bookAuthorId;
+    }
+
+    public void setBookAuthorId(Long bookAuthorId) {
+        this.bookAuthorId = bookAuthorId;
+    }
+
+    public Author getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(Author authorId) {
+        this.authorId = authorId;
+    }
+
+    public Book getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Book bookId) {
+        this.bookId = bookId;
+    }
+
+    public boolean isMainAuthor() {
+        return mainAuthor;
+    }
+
+    public void setMainAuthor(boolean mainAuthor) {
+        this.mainAuthor = mainAuthor;
+    }
 }
