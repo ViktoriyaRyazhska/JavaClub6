14aaaa877b0c3c2ffad4ec2ada810877ecae7f12
Taras Kadushkevych <kadtaras@gmail.com>
2022-04-18 22:20:52 +0300

    added model Role and User

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -1,4 +1,22 @@
 package model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+@Entity
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    @Column(name = "role")
+    @NotEmpty()
+    private String role;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
+
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,83 @@
 package model;
 
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name")
+    @NotEmpty()
+    private String userName;
+
+    @Column(name = "user_surname")
+    @NotEmpty()
+    private  String userSurname;
+
+    @Column(name = "user_email")
+    @NotEmpty()
+    private String userEmail;
+
+    @Column(name = "user_password")
+    @NotEmpty()
+    private String userPassword;
+
+    @Column(name = "user_date_register")
+    @NotEmpty()
+    private Date userDateRegister;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserSurname() {
+        return userSurname;
+    }
+
+    public void setUserSurname(String userSurname) {
+        this.userSurname = userSurname;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Date getUserDateRegister() {
+        return userDateRegister;
+    }
+
+    public void setUserDateRegister(Date userDateRegister) {
+        this.userDateRegister = userDateRegister;
+    }
 }
