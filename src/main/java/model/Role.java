ce83d92c8977d9931d07945cb6c103e1ba069bfd
Taras Kadushkevych <kadtaras@gmail.com>
2022-04-19 12:39:21 +0300

    edits in role

--- src/main/java/model/Role.java
+++ src/main/java/model/Role.java
@@ -19,4 +19,27 @@ public class Role {
     @OneToMany(mappedBy = "role")
     private Set<User> users;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
